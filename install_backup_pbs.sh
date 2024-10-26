3dcfc011e0a8a94046becbc9fb8c0176d34b9b3b
highTower.SU <3651850+highTowerSU@users.noreply.github.com>
2024-10-26 21:13:40 +0200

    Update install_backup_pbs.sh

--- install_backup_pbs.sh
+++ install_backup_pbs.sh
@@ -3,6 +3,7 @@
 # Installationspfade
 BACKUP_SCRIPT="/usr/local/bin/backup_pbs.sh"
 NOVALID_SCRIPT="/usr/local/bin/remove_no_valid_proxmox.sh"
+SYMLINK_PATH="/etc/cron.daily/remove-no-valid-proxmox"
 CONFIG_FILE="/etc/backup_pbs.conf"
 
 # Skript kopieren
@@ -10,10 +11,26 @@ echo "Installing backup script to $BACKUP_SCRIPT..."
 sudo cp backup_pbs.sh "$BACKUP_SCRIPT"
 sudo chmod +x "$BACKUP_SCRIPT"
 
-# Skript kopieren
-echo "Installing novalid script to $NOVALID_SCRIPT..."
-sudo cp remove_no_valid_proxmox.sh "$NOVALID_SCRIPT"
-sudo chmod +x "$NOVALID_SCRIPT"
+# Abfrage, ob der Symlink erstellt werden soll
+read -p "Do you want to install novalid script? (y/n): " install_novalid
+if [[ "$install_novalid" == "y" || "$install_novalid" == "Y" ]]; then
+    # Skript kopieren
+    echo "Installing novalid script to $NOVALID_SCRIPT..."
+    sudo cp remove_no_valid_proxmox.sh "$NOVALID_SCRIPT"
+    sudo chmod +x "$NOVALID_SCRIPT"
+
+    # Abfrage, ob der Symlink erstellt werden soll
+    read -p "Do you want to create a symlink for daily cron? (y/n): " create_symlink
+    if [[ "$create_symlink" == "y" || "$create_symlink" == "Y" ]]; then
+        echo "Creating symlink at $SYMLINK_PATH..."
+        ln -sf "$NOVALID_SCRIPT" "$SYMLINK_PATH"
+        echo "Symlink created."
+    else
+        echo "Symlink creation skipped."
+    fi
+else
+    echo "Install novalid script skipped."
+fi
 
 # Konfigurationsdatei kopieren
 if [ ! -f "$CONFIG_FILE" ]; then
