00f5193dbb6eb11dc883d03b7ca4403b5f3a2530
highTower.SU <3651850+highTowerSU@users.noreply.github.com>
2024-10-26 22:59:36 +0200

    Update remove_no_valid_proxmox.sh

--- remove_no_valid_proxmox.sh
+++ remove_no_valid_proxmox.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
+
+# -----------------------------------------------------------------------------
+# generate_banner.sh
+# Copyright (C) 2023 highTowerSU
+#
+# Repository: https://github.com/highTowerSU/pbs-backup
+#
+# Description: This script removes the Proxmox Enterprise subscription warning 
+# from the Proxmox interface. It modifies configuration files to suppress the 
+# alert about missing Enterprise subscriptions, enabling a cleaner user interface 
+# without the warning message.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# -----------------------------------------------------------------------------
+
 sed -Ezi.bak "s/(Ext.Msg.show\(\{\s+title: gettext\('No valid sub)/void\(\{ \/\/\1/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js
 for i in pveproxy.service proxmox-backup.service; do
   if systemctl list-units --full -all | grep -Fq ".service"; then
