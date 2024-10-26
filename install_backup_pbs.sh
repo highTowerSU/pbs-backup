50ec05f40623b3ec95939c756267a68a1364d3a0
highTower.SU <3651850+highTowerSU@users.noreply.github.com>
2024-10-26 21:05:33 +0200

    Update install_backup_pbs.sh

--- install_backup_pbs.sh
+++ install_backup_pbs.sh
@@ -2,12 +2,18 @@
 
 # Installationspfade
 BACKUP_SCRIPT="/usr/local/bin/backup_pbs.sh"
+NOVALID_SCRIPT="/usr/local/bin/remove_no_valid_proxmox.sh"
 CONFIG_FILE="/etc/backup_pbs.conf"
 
 # Skript kopieren
 echo "Installing backup script to $BACKUP_SCRIPT..."
 sudo cp backup_pbs.sh "$BACKUP_SCRIPT"
 sudo chmod +x "$BACKUP_SCRIPT"
+
+# Skript kopieren
+echo "Installing backup script to $NOVALID_SCRIPT..."
+sudo cp remove_no_valid_proxmox.sh "$NOVALID_SCRIPT"
+sudo chmod +x "$NOVALID_SCRIPT"
 
 # Konfigurationsdatei kopieren
 if [ ! -f "$CONFIG_FILE" ]; then
