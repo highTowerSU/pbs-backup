b5da5dde9f6753377b1fe527a509331eb93904bd
highTower.SU <3651850+highTowerSU@users.noreply.github.com>
2024-10-28 18:44:48 +0100

    Update remove_no_valid_proxmox.sh

--- remove_no_valid_proxmox.sh
+++ remove_no_valid_proxmox.sh
@@ -27,7 +27,7 @@
 
 sed -Ezi.bak "s/(Ext.Msg.show\(\{\s+title: gettext\('No valid sub)/void\(\{ \/\/\1/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js
 for i in pveproxy.service proxmox-backup.service; do
-  if systemctl list-units --full -all | grep -Fq ".service"; then
-    systemctl restart ;
+  if systemctl list-units --full -all | grep -Fq $i; then
+    systemctl restart $i;
   fi
 done
